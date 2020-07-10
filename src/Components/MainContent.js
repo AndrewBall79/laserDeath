781364a8bbf1751f71c10a06b39e672a8702ac47
Andrew Ball <andy.bob.ball@gmail.com>
2020-07-10 18:06:51 -0500

    first commit

---
+++ src/Components/MainContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Body() {
+    return (
+        <div className="body">
+            <h1>BODY</h1>
+            <h1>BODY</h1>
+            <h1>BODY</h1>
+            <h1>BODY</h1>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/Components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>ANDY B.</h1>
+            <p>There's a lot of stuff to say about me. </p>
+            <ul>
+                <li>place1</li>
+                <li>place2</li>
+                <li>place3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
