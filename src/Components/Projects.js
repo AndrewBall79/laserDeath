98722032f7258feadf839041526ce9f30aaf07c2
Andrew Ball <andy.bob.ball@gmail.com>
2020-07-16 15:27:24 -0500

    project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,7 @@ class Projects extends React.Component {
                 <br/>
                 <p className="project-header">{this.props.project.title}</p>
                 <a href={this.props.project.href}>
-                <img width="90%" src={this.props.project.url} alt={this.props.project.alt}/>
+                <img width="90%"  src={this.props.project.url} alt={this.props.project.alt}/>
                 </a>
                 <br/><br/>
                 <h5 className="projectsDescription">{this.props.project.description}</h5>
