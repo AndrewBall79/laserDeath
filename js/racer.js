9ad9b19dd23cefa37ef3d9f51b85756e8e62fbb7
Andrew Ball <andy.bob.ball@gmail.com>
2020-10-15 22:42:55 -0500

    trying to deconstruct the waves

--- js/racer.js
+++ js/racer.js
@@ -1,6 +1,10 @@
-var c = document.createElement("canvas");
+var c = document.getElementById("canvas");
+
 var ctx = c.getContext('2d');
-c.width = 500;
+var ctx2 = c.getContext('2d');
+
+
+c.width = innerWidth;
 c.height = 350;
 document.body.appendChild(c);
 
@@ -16,54 +20,66 @@ var noise = x=>{
     return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
 };
 
-var player = new function() {
-    this.x = c.width/2;
-    this.y = 0;
-    this.ySpeed = 0;
-    this.rot = 0;
-    this.rSpeed = 0;
+var perm2 = [];
+while (perm2.length < 255){
+    while (perm2.includes(val2 = Math.floor(Math.random()*255))) ;
+    perm2.push(val2);
+}
 
-    this.img = new Image();
-    this.img.src = "../images/SVG-Path/moto.png";
-    this.draw = function (){
-        var p1 = c.height - noise(t + this.x) * 0.25;
-        var p2 = c.height - noise(t+5 + this.x) * 0.25;
-
-        var grounded = 0;
-        if (p1-15 > this.y){
-            this.ySpeed += 0.1;
-        } else {
-            this.ySpeed -= this.y - (p1-15);
-            this.y = p1 - 15;
-            grounded = 1;
-        }
-
-        if(!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5 ){
-            playing = false;
-            this.rSpeed = 5;
-            k.ArrowUp = 1;
-            this.x -= speed * 5;
-        }
-
-        var angle = Math.atan2((p2-15) - this.y, (this.x+5) - this.x);
-        this.y += this.ySpeed;
-
-        if (grounded && playing) {
-            this.rot -= (this.rot - angle) * 0.5;
-            this.rSpeed = this.rSpeed - (angle - this.rot);
-        }
-        this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
-        this.rot -= this.rSpeed * 0.1;
-        if(this.rot > Math.PI) this.rot = -Math.PI;
-        if(this.rot < -Math.PI) this.rot = Math.PI;
-        this.rot = angle;
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.rot);
-        ctx.drawImage(this.img, -15, -15, 30, 30);
-        ctx.restore()
-    }
+var lerp2 = (a, b, t) => a + (b-a) * (1-Math.cos(t*Math.PI))/2;
+var noise2 = x=>{
+    x = x * 0.01 % 25;
+    return lerp2(perm2[Math.floor(x)], perm2[Math.ceil(x)], x - Math.floor(x));
 };
+
+// var player = new function() {
+//     this.x = c.width/2;
+//     this.y = 0;
+//     this.ySpeed = 0;
+//     this.rot = 0;
+//     this.rSpeed = 0;
+//
+//     this.img = new Image();
+//     this.img.src = "../images/SVG-Path/moto.png";
+//     this.draw = function (){
+//         var p1 = c.height - noise(t + this.x) * 0.25;
+//         var p2 = c.height - noise(t+5 + this.x) * 0.25;
+//
+//         var grounded = 0;
+//         if (p1-15 > this.y){
+//             this.ySpeed += 0.1;
+//         } else {
+//             this.ySpeed -= this.y - (p1-15);
+//             this.y = p1 - 15;
+//             grounded = 1;
+//         }
+//
+//         if(!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5 ){
+//             playing = false;
+//             this.rSpeed = 5;
+//             k.ArrowUp = 1;
+//             this.x -= speed * 5;
+//         }
+//
+//         var angle = Math.atan2((p2-15) - this.y, (this.x+5) - this.x);
+//         this.y += this.ySpeed;
+//
+//         if (grounded && playing) {
+//             this.rot -= (this.rot - angle) * 0.5;
+//             this.rSpeed = this.rSpeed - (angle - this.rot);
+//         }
+//         this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
+//         this.rot -= this.rSpeed * 0.1;
+//         if(this.rot > Math.PI) this.rot = -Math.PI;
+//         if(this.rot < -Math.PI) this.rot = Math.PI;
+//         this.rot = angle;
+//         ctx.save();
+//         ctx.translate(this.x, this.y);
+//         ctx.rotate(this.rot);
+//         ctx.drawImage(this.img, -15, -15, 30, 30);
+//         ctx.restore()
+//     }
+// };
 
 var t = 0;
 var speed = 0;
@@ -71,7 +87,7 @@ var playing = true;
 var k = {ArrowUp:0, ArrowDown:0, ArrowLeft:0, ArrowRight:0};
 function loop() {
     speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.01;
-    t += 10 * speed;
+    t += 10;
     ctx.fillStyle = "#19f";
     ctx.fillRect(0, 0, c.width, c.height);
     ctx.fillStyle = "black";
@@ -79,16 +95,24 @@ function loop() {
     ctx.moveTo(0, c.height);
     for (let i = 0; i < c.width; i++)
         ctx.lineTo(i, c.height - noise(t + i) * 0.25);
-
-    ctx.lineTo(c.width, c.height);
-    ctx.fill();
-
-    player.draw();
+    // player.draw();
     requestAnimationFrame(loop);
 }
 
+var t2 = 0;
+function loop2(){
+    t2 += 5;
+    ctx2.fillStyle = "orange";
+    for (let i = 0; i < c.width; i++)
+        ctx2.lineTo(i, c.height - noise2(t2 + i) * 0.95);
+    ctx2.lineTo(c.width, c.height);
+    ctx2.fill();
+    requestAnimationFrame(loop2);
+
+}
 onKeydown = d=> k[d.key] = 1;
 onKeyup = d=> k[d.key] = 0 ;
 
 
 loop();
+loop2();
