8f30cd14d770175f167f3f9979d18ccf0561ed73
Andrew Ball <andy.bob.ball@gmail.com>
2020-11-02 14:01:36 -0600

    quote generator api call

---
+++ js/align2.js
@@ -0,0 +1,23 @@
+let btn = document.getElementById('btn');
+
+let request = new XMLHttpRequest();
+request.open("GET", "");
+request.send();
+
+
+const url = "https://api.quotable.io/random";
+setInterval(generateQuote, 8000);
+function generateQuote() {
+    fetch(url)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (data) {
+            document.getElementById("quote").innerHTML = data.content;
+            document.getElementById("author").innerHTML = "- " + data.author;
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+
